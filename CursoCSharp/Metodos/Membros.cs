fc9549d37b255049be004ae9220160c927b97274
thaisascastro <thaisascastro@gmail.com>
2021-04-29 12:38:09 -0300

    Atributos e metodos

--- CursoCSharp/Metodos/Membros.cs
+++ CursoCSharp/Metodos/Membros.cs
@@ -5,7 +5,23 @@ namespace CursoCSharp.Metodos
      {
          public static void Executar()
         {
+            Pessoa sicrano = new Pessoa();
+            sicrano.Nome = "Thaísa";
+            sicrano.Idade = 21;
 
+            //Console.WriteLine($"{sicrano.Nome}, têm {sicrano.Idade} anos.");
+
+            sicrano.ApresentarNoConsole();
+            sicrano.Zerar();
+            sicrano.ApresentarNoConsole();
+
+            var fulano = new Pessoa();
+            fulano.Nome = "Leandro";
+            fulano.Idade = 20;
+
+            var apresentacaoDoFulano = fulano.Apresentar();
+            Console.WriteLine(apresentacaoDoFulano.Length);
+            Console.WriteLine(apresentacaoDoFulano);
         }
          
      }
--- CursoCSharp/Metodos/Pessoa.cs
+++ CursoCSharp/Metodos/Pessoa.cs
@@ -5,5 +5,23 @@ namespace CursoCSharp.Metodos
     {
         // visível
         public string Nome;
+        public int Idade;
+
+        public string Apresentar()
+        {
+            return string.Format($"Olá! Me chamo {Nome}, tenho {Idade} anos.");
+        }
+
+        public void ApresentarNoConsole()
+        {
+            Console.WriteLine(Apresentar());
+        }
+
+        public void Zerar()
+        {
+            Nome = "";
+            Idade = 0;
+        }
+
     }
 }
