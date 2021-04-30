d00f0131359b266c7dc9729f6d5f430d0c3e17ba
thaisascastro <thaisascastro@gmail.com>
2021-04-30 12:18:10 -0300

    Metodo com Retorno

--- CursoCSharp/Metodos/MetodosComRetorno.cs
+++ CursoCSharp/Metodos/MetodosComRetorno.cs
@@ -24,6 +24,40 @@ namespace CursoCSharp.Metodos
         }
     }
 
+    class CalculadoraCadeia
+    {
+        int memoria;
+
+        public CalculadoraCadeia Somar(int a)
+        {
+            memoria += a;
+            return this;
+        }
+
+        public CalculadoraCadeia Multiplicar(int a)
+        {
+            memoria *= a;
+            return this;
+        }
+
+        public CalculadoraCadeia Limpar()
+        {
+            memoria = 0;
+            return this;
+        }
+
+        public CalculadoraCadeia Imprimir()
+        {
+            Console.WriteLine(memoria);
+            return this;
+        }
+
+        public int Resultado()
+        {
+            return memoria;
+        } 
+    }
+
     class MetodosComRetorno
     {
         public static void Executar()
@@ -35,6 +69,12 @@ namespace CursoCSharp.Metodos
             Console.WriteLine(calculadoraComum.Subtrair(2, 7));
             Console.WriteLine(calculadoraComum.Multiplicar(8, 7));
             Console.WriteLine(calculadoraComum.Dividir(40, 4));
+
+            var calculadoraCadeia = new CalculadoraCadeia();
+            calculadoraCadeia.Somar(3).Multiplicar(5).Imprimir().Limpar().Imprimir();
+
+            resultado = calculadoraCadeia.Somar(3).Multiplicar(7).Resultado();
+            Console.WriteLine(resultado);
         }
     }
 }
