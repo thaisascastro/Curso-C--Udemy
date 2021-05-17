801a42762bb7c255095416b169ebfb52aa16a386
thaisascastro <thaisascastro@gmail.com>
2021-05-17 16:04:37 -0300

    definindo modo static com class

--- CursoCSharp/Metodos/AtributosEstaticos.cs
+++ CursoCSharp/Metodos/AtributosEstaticos.cs
@@ -4,15 +4,46 @@ namespace CursoCSharp.Metodos
     public class Produto
     {
         public string Nome;
-        public int Preco;
-        public double Desconto = 0.1;
+        public double Preco;
+        public static double Desconto = 0.1;
+
+        public Produto(string nome, double preco, double desconto)
+        {
+            Nome = nome;
+            Preco = preco;
+            Desconto = desconto;
+        }
+
+        public Produto()
+        {
+
+        }
+
+        public double CalcularDesconto()
+        {
+            return Preco - Preco * Desconto;
+        }
 
     }
      class AtributosEstaticos
     {
         public static void Executar()
         {
-            Console.WriteLine("Atributos Estáticos");
+            var produto1 = new Produto("Canenta", 3.2, 0.1);
+
+            var produto2 = new Produto() {
+                Nome = "Lápis",
+                Preco = 5.3,
+        };
+
+            Produto.Desconto = 0.5;
+            Console.WriteLine("Preço com desconto: {0}", produto1.CalcularDesconto());
+            Console.WriteLine("Preço com desconto: {0}", produto2.CalcularDesconto());
+
+            // agora classe Desconto permance static, assim todos passaram a conter desconto 0.1, pois pertence a classe desconto.
+            Produto.Desconto = 0.02;
+            Console.WriteLine("Preço com desconto: {0}", produto1.CalcularDesconto());
+            Console.WriteLine("Preço com desconto: {0}", produto2.CalcularDesconto());
         }
     }
 }
