624ba5f3840343777f23840fb4acc8ca14b3cb4b
thaisascastro <thaisascastro@gmail.com>
2021-05-17 16:45:49 -0300

    Parametros Variaveis

--- CursoCSharp/Metodos/ParametrosVariaveis.cs
+++ CursoCSharp/Metodos/ParametrosVariaveis.cs
@@ -3,9 +3,17 @@ namespace CursoCSharp.Metodos
 {
     class ParametrosVariaveis
     {
+        public static void Recepcionar(params string[] pessoas)
+        {
+            foreach (var pessoa in pessoas)
+            {
+                Console.WriteLine("Olá {0}.", pessoa);
+            }
+        }
+
        public static void Executar()
         {
-
+            Recepcionar("Thaísa", "Leandro", "Bia", "Jusi", "Osvaldo", "Rato");
         }
     }
 }
