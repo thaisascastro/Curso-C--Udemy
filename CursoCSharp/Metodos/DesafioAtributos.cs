88868ec88b7b5f40c146d1466c19e2f2d937032a
thaisascastro <thaisascastro@gmail.com>
2021-05-17 16:30:01 -0300

    acessando atributo estatico instanciando

---
+++ CursoCSharp/Metodos/DesafioAtributos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CursoCSharp.Metodos
+{
+    class DesafioAtributos
+    {
+        int a = 10;
+        public static void Executar()
+        {
+            DesafioAtributos desafio = new DesafioAtributos();
+            Console.WriteLine(desafio.a);
+        }
+    }
+}
